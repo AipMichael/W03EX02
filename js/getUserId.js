70944bbc1f674077ef58c42629dc93a48225be80
AipBorges <mpbds@uic.es>
2021-10-13 12:57:34 +0200

    first function getUserId done with error

--- js/getUserId.js
+++ js/getUserId.js
@@ -1,20 +1,23 @@
 let id;
 
-const getUserId = (named) =>{
-  id = Math.floor(Math.random() * 151);
-  if (named === "Luis") {
-    return new Error("I'm sick of you!");
-  } else {
-    return `Your ID is ${id}`;
-  }
-}
-
-const promiseMeTheId = (name) => {
+const promiseMeTheId = (named) => {
   const promise = new Promise ( (resolve, reject) =>
-  setTimeout( () => {
-    const giveUserId = getUserId(name);
-    resolve(giveUserId)
-  }, 2000));
+    setTimeout( () => {
+      
+    id = Math.floor(Math.random() * 151);
+    if (named === "Luis") {
+      
+      reject("I'm sick of you!");
+    } else {
+      
+      resolve(`Your ID is ${id}`);
+    }
+  
+    }, 2000));
     return promise;
 }
 
+
+const callAndGetTheId = promiseMeTheId("Pia");
+
+callAndGetTheId.then((resolvedValue)=>console.log(resolvedValue));
